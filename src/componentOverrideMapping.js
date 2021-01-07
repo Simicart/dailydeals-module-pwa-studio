1c5b87f1de671449e8ba633bb64496327a22b690
Cody Nguyen <cody@simicart.com>
2021-01-07 10:29:49 +0700

    Fix

--- src/componentOverrideMapping.js
+++ src/componentOverrideMapping.js
@@ -1,8 +1,5 @@
 module.exports = componentOverride = {
-	[`@magento/venia-ui/lib/components/ProductFullDetail/productFullDetail.js`]: '@simicart/dailydeals-module-pwa-studio
-/src/override/productFullDetail.js',
-	[`@magento/venia-ui/lib/components/Gallery/item.js`]: '@simicart/dailydeals-module-pwa-studio
-/src/override/item.js',
-	[`@magento/venia-ui/lib/components/CategoryList/categoryList.js`]: '@simicart/dailydeals-module-pwa-studio
-/src/override/categoryList.js'
+	[`@magento/venia-ui/lib/components/ProductFullDetail/productFullDetail.js`]: '@simicart/dailydeals/src/override/productFullDetail.js',
+	[`@magento/venia-ui/lib/components/Gallery/item.js`]: '@simicart/dailydeals/src/override/item.js',
+	[`@magento/venia-ui/lib/components/CategoryList/categoryList.js`]: '@simicart/dailydeals/src/override/categoryList.js'
 };
